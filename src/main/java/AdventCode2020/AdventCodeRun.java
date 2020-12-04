3e99f03acc947304ac39ee38d08b929138f83d73
Fangyi Streitel <fangyi.streitel@gmail.com>
2020-12-04 08:15:54 +0100

    Day03 commit

--- src/main/java/AdventCode2020/AdventCodeRun.java
+++ src/main/java/AdventCode2020/AdventCodeRun.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 
 public class AdventCodeRun {
     public static void main(String[] args) throws IOException {
-        day02 instance_day02 = new day02();
-        instance_day02.solve();
 
+        day03 instance_day03 = new day03();
+        instance_day03.solve();
+/*        day02 instance_day02 = new day02();
+        instance_day02.solve();
+*/
 /*           day01_1 instance_day01_1 = new day01_1();
            int sum = 2020;
            instance_day01_1.solve(sum);
--- src/main/java/AdventCode2020/day03.java
+++ src/main/java/AdventCode2020/day03.java
@@ -1,2 +1,45 @@
-package AdventCode2020;public class day03 {
+package AdventCode2020;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class day03 {
+
+    public void solve() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/Inputs/day03.txt"));
+        String line;
+        int lineNumber = 0;
+
+
+        int slope_x = 7;
+        int slope_y = 1;
+
+        int index_x_initial = slope_x;
+        int index_y_initial = slope_y;
+
+        int index_x_next = index_x_initial;
+        int index_y_next = index_y_initial;
+
+        int count_tree = 0;
+        while ((line = bufferedReader.readLine()) != null){
+
+            if(lineNumber == index_y_next){
+                index_y_next = index_y_next + slope_y;
+                System.out.println(line.charAt(index_x_next));
+                if(line.charAt(index_x_next) == '#'){
+                    count_tree++;
+                }
+                index_x_next = (index_x_next + slope_x)%line.length();
+                //System.out.println(String.valueOf(index_x_next));
+            }
+
+            lineNumber++;
+
+        }
+
+        System.out.println(String.valueOf(count_tree));
+
+    }
 }
