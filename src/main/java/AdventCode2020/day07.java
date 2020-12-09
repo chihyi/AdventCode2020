adc5e4e0b4040d5a9df67971690e9c2f07b76b04
Fangyi Streitel <fangyi.streitel@gmail.com>
2020-12-09 21:45:04 +0100

    Updated part01 day07

--- src/main/java/AdventCode2020/day07.java
+++ src/main/java/AdventCode2020/day07.java
@@ -6,42 +6,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class day07 {
 
     private HashMap<String, Bag> hashMap_Bags = new HashMap<>();
-    private HashMap<String, BagNode> hashMap_BagNodes =  new HashMap<>();
+    private HashSet<String> hashSet_found = new HashSet<>();
+    private HashSet<String> hashSet_toDo = new HashSet<>();
+
+    private String contain_bag = "shiny gold";
 
     public void solve() throws IOException {
         readFile();
 
-        createBagNodes();
 
-        for(Map.Entry<String, BagNode> bagNodeEntry: hashMap_BagNodes.entrySet()){
-            BagNode bagNode = bagNodeEntry.getValue();
-            Bag bag = hashMap_Bags.get(bagNodeEntry.getKey());
-
-            for(int i=0;i<bag.children_color.size();i++){
-
+        hashSet_toDo.add(contain_bag);
+        while (!hashSet_toDo.isEmpty()){
+            String target_color = hashSet_toDo.iterator().next();
+            for(Map.Entry<String, Bag> me:hashMap_Bags.entrySet()){
+                Bag bag = me.getValue();
+                if(bag.children.containsKey(target_color)){
+                    hashSet_toDo.add(bag.color);
+                    hashSet_found.add(bag.color);
+                    System.out.println(bag.color);
+                }
             }
-
+            hashSet_toDo.remove(target_color);
         }
 
-    }
+        System.out.println(hashSet_found.size());
 
-    private void createBagNodes(){
-        for(Map.Entry<String, Bag> bag_entry:hashMap_Bags.entrySet()){
-            BagNode bagNode = new BagNode(bag_entry.getKey());
-            hashMap_BagNodes.put(bag_entry.getKey(), bagNode);
-        }
     }
 
     private void readFile() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/Inputs/day07.txt"));
         String line;
         while ((line=bufferedReader.readLine())!=null){
-            System.out.println(line);
+            //System.out.println(line);
 
             Bag bag = new Bag();
 
@@ -53,12 +55,10 @@ public class day07 {
             String[] split_children = children_bag.split(",");
             for(int i=0;i<split_children.length;i++){
                 String[] split = split_children[i].split(" ");
-                System.out.println(split.length);
                 if(split.length>=5) {
                     Integer child_number = Integer.valueOf(split[1]);
                     String child_color = split[2] + " " + split[3];
-                    bag.children_color.add(child_color);
-                    bag.children_number.add(child_number);
+                    bag.children.put(child_color, child_number);
                 }
             }
             hashMap_Bags.put(parent_bag_color, bag);
@@ -68,43 +68,8 @@ public class day07 {
 
     private class Bag{
         public String color = null;
-        public ArrayList<Integer> children_number = new ArrayList<>();
-        public ArrayList<String> children_color = new ArrayList<>();
+        public HashMap<String, Integer> children = new HashMap<>();
     }
 
-    private class BagNode {
-        private String color = null;
-        private BagNode parent = null;
-        private ArrayList<BagNode> children = new ArrayList<>();
 
-        public BagNode(String color){
-            this.color = color;
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-        public void setColor(String color) {
-            this.color = color;
-        }
-
-        public BagNode getParent() {
-            return parent;
-        }
-
-        public void setParent(BagNode parent) {
-            this.parent = parent;
-        }
-
-        public ArrayList<BagNode> getChildren() {
-            return children;
-        }
-
-        public BagNode addChild(BagNode child){
-            child.setParent(this);
-            this.children.add(child);
-            return child;
-        }
-    }
 }
