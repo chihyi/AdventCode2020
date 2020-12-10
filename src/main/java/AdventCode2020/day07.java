fd5f19e95fb83a608637d61012cab1e67dcabe9a
Fangyi Streitel <fangyi.streitel@gmail.com>
2020-12-10 21:14:16 +0100

    Updated part 2 day07

--- src/main/java/AdventCode2020/day07.java
+++ src/main/java/AdventCode2020/day07.java
@@ -16,13 +16,15 @@ public class day07 {
     private HashSet<String> hashSet_toDo = new HashSet<>();
 
     private String contain_bag = "shiny gold";
+    private Integer num_bags = 0;
 
     public void solve() throws IOException {
         readFile();
 
 
         hashSet_toDo.add(contain_bag);
-        while (!hashSet_toDo.isEmpty()){
+        // Part 1 solution
+        /*while (!hashSet_toDo.isEmpty()){
             String target_color = hashSet_toDo.iterator().next();
             for(Map.Entry<String, Bag> me:hashMap_Bags.entrySet()){
                 Bag bag = me.getValue();
@@ -35,8 +37,36 @@ public class day07 {
             hashSet_toDo.remove(target_color);
         }
 
-        System.out.println(hashSet_found.size());
+        System.out.println(hashSet_found.size());*/
 
+        // Part02 solution
+        /*int multiple_base = 1;
+        while (!hashSet_toDo.isEmpty()){
+            String target_color = hashSet_toDo.iterator().next();
+            Bag bag = hashMap_Bags.get(target_color);
+            for(Map.Entry<String, Integer> me:bag.children.entrySet()){
+                num_bags += me.getValue() * multiple_base;
+                hashSet_toDo.add(me.getKey());
+            }
+            hashSet_toDo.remove(target_color);
+        }
+        System.out.println(num_bags);*/
+        System.out.println(num_bags_inside(hashMap_Bags.get(contain_bag)) - 1);
+
+
+    }
+
+    private Integer num_bags_inside(Bag bag){
+        if(bag.children.isEmpty()){
+            return 1;
+        }else{
+            int sum = 0;
+            for(Map.Entry<String, Integer> me: bag.children.entrySet()){
+                Bag child = hashMap_Bags.get(me.getKey());
+                sum += me.getValue() * num_bags_inside(child) ;
+            }
+            return sum + 1;
+        }
     }
 
     private void readFile() throws IOException {
