789f10e60583bc7f134457cdba4e61ddd0d21053
Fangyi Streitel <fangyi.streitel@gmail.com>
2020-12-17 22:11:26 +0100

    Update day14 part2 solution

--- src/main/java/AdventCode2020/day14.java
+++ src/main/java/AdventCode2020/day14.java
@@ -24,24 +24,49 @@ public class day14 {
                 String[] split_mem_2 = split_mem_1[1].split("\\]");
                 String address_str = split_mem_2[0];
                 String value_str = split[1];
-                long value = Long.valueOf(value_str);
-                String value_binary = String.format("%36s", Long.toBinaryString(value)).replace(' ', '0');
-                //System.out.println(mask);
-                //System.out.println(value_binary);
+                long value = Long.parseLong(value_str);
+                long address = Long.valueOf(address_str);
+                String address_binary = String.format("%36s", Long.toBinaryString(address)).replace(' ', '0');
+                System.out.println(mask);
+                System.out.println(address_binary);
 
+                int n_X = 0;
                 for(int i=0;i<mask.length();i++){
-                    if(mask.charAt(i) == '0'){
-                        value_binary = changeCharInPosition(i, '0', value_binary);
+                    if(mask.charAt(i) == 'X'){
+                        address_binary = changeCharInPosition(i, 'X', address_binary);
+                        n_X++;
                     }else if(mask.charAt(i) == '1'){
-                        value_binary = changeCharInPosition(i, '1', value_binary);
+                        address_binary = changeCharInPosition(i, '1', address_binary);
                     }
                 }
-                value = Long.parseLong(value_binary,  2);
 
-                if(!memory.containsKey(Long.parseLong(address_str))){
-                    memory.put(Long.parseLong(address_str), value);
-                }else{
-                    memory.replace(Long.parseLong(address_str), value);
+                System.out.println(address_binary);
+                System.out.println(n_X);
+
+                for(int i=0;i<Math.pow(2, n_X);i++){
+                    String address_binary_new = address_binary;
+                    String i_str = Integer.toBinaryString(i);
+                    int n_lacked_zeros = n_X-i_str.length();
+                    for(int j=0;j<n_lacked_zeros;j++){
+                        i_str = "0" + i_str;
+                    }
+                    System.out.println(i_str);
+                    int i_X = 0;
+                    for(int k=0;k<address_binary.length();k++){
+                        if(address_binary.charAt(k) == 'X'){
+                            address_binary_new = changeCharInPosition(k, i_str.charAt(i_X), address_binary_new);
+                            i_X++;
+                        }
+                    }
+                    System.out.println(address_binary_new);
+                    System.out.println(i_X);
+                    System.out.println(n_X);
+                    if(!memory.containsKey(Long.parseLong(address_binary_new, 2))){
+                        memory.put(Long.parseLong(address_binary_new, 2), value);
+                    }else{
+                        memory.replace(Long.parseLong(address_binary_new, 2), value);
+                    }
+
                 }
             }
 
